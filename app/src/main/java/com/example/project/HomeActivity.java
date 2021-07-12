a3b4acdb0125e2fef425b9ca62b6a18177cf425f
Joel Figueroa <jfigueroa@factexp.site>
2021-07-11 20:20:53 -0500

    LoginMenu

--- app/src/main/java/com/example/project/HomeActivity.java
+++ app/src/main/java/com/example/project/HomeActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -14,8 +18,37 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
     }
 
-    public void buttonExit(View view){
-        startActivity(new Intent(this,LoginActivity.class));
-        finish();
+    public boolean onCreateOptionsMenu(Menu menu)  {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+
+            case R.id.configuracion:
+                Toast.makeText(this, "SELECIONO CONFIGURACION", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.contacto:
+                Toast.makeText(this, "SELECIONO CONTACTO", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.idioma:
+                Toast.makeText(this, "SELECIONO IDIOMA", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.acerca:
+                Toast.makeText(this, "SELECIONO Acerca", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.salir:
+                startActivity(new Intent(this,LoginActivity.class));
+                finish();
+            default:
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
 }
